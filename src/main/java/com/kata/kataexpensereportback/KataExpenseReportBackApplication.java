53b895a540bd6dd991b82a7f2cb43358c24e5b76
Antoine MARTIN <antoinemartin@MacBookPro-M1-de-Antoine-Martin.local>
2025-06-23 18:32:46 +0200

    Initialisation BackEnd

---
+++ src/main/java/com/kata/kataexpensereportback/KataExpenseReportBackApplication.java
@@ -0,0 +1,13 @@
+package com.kata.kataexpensereportback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataExpenseReportBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataExpenseReportBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kata/kataexpensereportback/KataExpenseReportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.kataexpensereportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataExpenseReportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
