851d36dd7306e8872b0c1ec696f36f6d69bff81c
Antoine MARTIN <antoinemartin@MacBookPro-M1-de-Antoine-Martin.local>
2025-06-25 11:25:36 +0200

    Ajout TU sur ExpenseId

---
+++ src/test/java/com/kata/kataexpensereportback/domain/ExpenseIdTest.java
@@ -0,0 +1,33 @@
+package com.kata.kataexpensereportback.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class ExpenseIdTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void SHOULD_throw_Exception_WHEN_value_is_less_than_one(int value) {
+        //GIVEN
+
+        //WHEN //THEN
+        assertThatIllegalArgumentException().isThrownBy(() -> new ExpenseId(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 1000})
+    void SHOULD_create_an_expense_id_WHEN_value_is_greater_than_one(int value) {
+        //GIVEN
+
+        //WHEN
+        ExpenseId expenseId = new ExpenseId(value);
+
+        //THEN
+        assertThat(expenseId.value()).isEqualTo(value);
+    }
+
+}
